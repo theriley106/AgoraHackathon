c97d6ce5d797052074cdef3eb1aef1bec26b9057
Theriley106 <christopherlambert106@gmail.com>
2018-11-30 15:00:29 -0500

    Updated api key

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, url_for, redirect, Markup, jsonify, make_response, send_from_directory, session
-from keys import *
+import os
+
 app = Flask(__name__, static_url_path='/static')
 
 
 @app.route('/', methods=['GET'])
 def index():
+	API_KEY = os.environ.get('API_KEY', None)
 	return render_template("index.html", API_KEY=AGORA_KEY)
 
 if __name__ == '__main__':
